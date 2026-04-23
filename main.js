13d206350ab4948386a61af6188d34562612aebc
Aly Nagy <alynagy78@gmail.com>
2026-04-23 21:08:59 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,41 @@
+// Question 1
+var answer1 = window.prompt("What is the capital of France?");
+
+if (answer1 == "paris") {
+    window.alert("Correct Answer!");
+    console.log("Q1: true");
+} else {
+    window.alert("Wrong Answer.");
+    console.log("Q1: false");
+}
+// age check
+var age = window.prompt("Enter your age:");
+
+if (age > 17) {
+    window.alert("You are an adult!");
+} else {
+    window.alert("you are too young to countinue with the game.");
+}
+
+// Question 2
+var answer2 = window.prompt("الفيل اكبر ولا الزرافه اطول؟");
+
+if (answer2 == "الفهد اسرع") {
+    window.alert("عرفت ازاي؟؟");
+    console.log("Q2: true");
+} else {
+    window.alert("غلط");
+    console.log("Q2: false");
+}
+// Question 3
+var answer3 = window.prompt("ايه هي اكبر دوله في العالم؟");
+
+if (answer3 == "روسيا") {
+    window.alert("صح");
+    console.log("Q3: true");
+} else {
+    window.alert("غلط");
+    console.log("Q3: false");
+}
+
+window.alert("Game Over :)");
